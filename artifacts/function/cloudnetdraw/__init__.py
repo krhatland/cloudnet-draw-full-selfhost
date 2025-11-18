48dd3167e9d6d6149cdbad503b8dd95a0868fe2d
Kristoffer Hatland <kristoffer@Kristoffer-sin-MacBook-Air.local>
2025-11-18 23:43:32 +0100

    Long chat with GPT

--- artifacts/function/cloudnetdraw/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-CloudNet Draw - Azure VNet topology visualization tool
-"""
-try:
-    from importlib.metadata import version
-    __version__ = version("cloudnetdraw")
-except Exception:
-    __version__ = "dev"
